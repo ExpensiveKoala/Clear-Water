80b87bfe88e59bed6ac4818d9e1a542144ceeadb
ExpensiveKoala <mmc_mmc_mmc@hotmail.com>
2018-01-25 00:00:11 -0500

    Create Mod

---
+++ src/main/java/koala/clarityreborn/ClarityReborn.java
@@ -0,0 +1,31 @@
+package koala.clarityreborn;
+
+import net.minecraft.block.material.Material;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * @author ExpensiveKoala
+ */
+@Mod(modid = "clarityreborn", version = "1.0")
+public class ClarityReborn {
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public void onRenderFog(EntityViewRenderEvent.FogDensity event) {
+        if(event.getState().getMaterial() == Material.WATER) {
+            event.setDensity(0);
+            event.setCanceled(true);
+        }
+    }
+}
