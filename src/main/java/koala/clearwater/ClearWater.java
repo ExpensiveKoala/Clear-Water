c3c1dfa438fd7bd7517f67006e7ffa8adfcdfe48
ExpensiveKoala <mmc_mmc_mmc@hotmail.com>
2019-08-05 19:53:25 -0400

    Update to 1.14.4

--- src/main/java/koala/clearwater/ClearWater.java
+++ src/main/java/koala/clearwater/ClearWater.java
@@ -1,47 +1,50 @@
 package koala.clearwater;
 
-import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.GlStateManager;
+import com.mojang.blaze3d.platform.GlStateManager;
+import net.minecraft.tags.FluidTags;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.config.ModConfig;
 
 /**
  * @author ExpensiveKoala
  */
-@Mod(modid = "clearwater", version = "1.2", name = "Clear Water")
+@Mod("clearwater")
 public class ClearWater {
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent
-    public void onRenderFog(EntityViewRenderEvent.FogDensity event) {
-        if(Configs.enableWater && event.getState().getMaterial() == Material.WATER) {
-            GlStateManager.setFog(GlStateManager.FogMode.EXP);
-            event.setDensity((float)Configs.fogDensityWater);
-            event.setCanceled(true);
-        } else if(Configs.enableLava && event.getState().getMaterial() == Material.LAVA) {
-            GlStateManager.setFog(GlStateManager.FogMode.EXP);
-            event.setDensity((float)Configs.fogDensityLava);
-            event.setCanceled(true);
-        }
-    }
-    
-    @SubscribeEvent
-    public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
-        if(event.getModID().equals("clearwater")) {
-            ConfigManager.sync("clearwater", Config.Type.INSTANCE);
-        }
-    }
+	
+	public ClearWater() {
+		MinecraftForge.EVENT_BUS.register(this);
+		
+		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Configs.clientSpec);
+	}
+	
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public void onRenderFog(EntityViewRenderEvent.FogDensity event) {
+		if (Configs.CLIENT.enableWater.get() && event.getInfo().getFluidState().isTagged(FluidTags.WATER)) {
+			GlStateManager.fogMode(GlStateManager.FogMode.EXP);
+			event.setDensity(Configs.CLIENT.fogDensityWater.get().floatValue());
+			event.setCanceled(true);
+		} else if (Configs.CLIENT.enableLava.get() && event.getInfo().getFluidState().isTagged(FluidTags.LAVA)) {
+			GlStateManager.fogMode(GlStateManager.FogMode.EXP);
+			event.setDensity(Configs.CLIENT.fogDensityLava.get().floatValue());
+			event.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public void onConfigChanged(ModConfig.ModConfigEvent event) {
+		final ModConfig config = event.getConfig();
+		if (config.getSpec() == Configs.clientSpec) {
+			Configs.enableWater = Configs.CLIENT.enableWater.get();
+			Configs.fogDensityWater = Configs.CLIENT.fogDensityWater.get();
+			Configs.enableLava = Configs.CLIENT.enableLava.get();
+			Configs.fogDensityLava = Configs.CLIENT.fogDensityLava.get();
+		}
+	}
 }
--- src/main/java/koala/clearwater/Configs.java
+++ src/main/java/koala/clearwater/Configs.java
@@ -1,23 +1,64 @@
 package koala.clearwater;
 
-import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * @author ExpensiveKoala
  */
-@Config(modid = "clearwater")
+
 public class Configs {
-    @Config.Comment("Enables modifications of fog under water")
-    public static boolean enableWater = true;
-
-    @Config.RangeDouble(min = 0, max = 5)
-    @Config.Comment("Value for fog density (0-5) Vanilla is 0.1")
-    public static double fogDensityWater = 0.0f;
-
-    @Config.Comment("Enables modifications of fog under lava")
-    public static boolean enableLava = true;
-
-    @Config.RangeDouble(min = 0, max = 5)
-    @Config.Comment("Value for fog density (0-5) Vanilla is 2.0")
-    public static double fogDensityLava = 0.2f;
+	
+	public static boolean enableWater;
+	public static double fogDensityWater;
+	public static boolean enableLava;
+	public static double fogDensityLava;
+	
+	public static class Client {
+		
+		public final ForgeConfigSpec.BooleanValue enableWater;
+		
+		public final ForgeConfigSpec.DoubleValue fogDensityWater;
+		
+		public final ForgeConfigSpec.BooleanValue enableLava;
+		
+		public final ForgeConfigSpec.DoubleValue fogDensityLava;
+		
+		Client(ForgeConfigSpec.Builder builder) {
+			builder.comment("Client Settings")
+			  .push("client");
+			
+			enableWater = builder
+			  .comment("Enables modifications of fog under water")
+			  .translation("")
+			  .define("enableWater", true);
+			
+			fogDensityWater = builder
+			  .comment("Value for fog density (0-5) Vanilla is 0.1")
+			  .translation("")
+			  .defineInRange("fogDensityWater", 0.0025, 0, 5);
+			
+			enableLava = builder
+			  .comment("Enables modifications of fog under lava")
+			  .translation("")
+			  .define("enableLava", true);
+			
+			fogDensityLava = builder
+			  .comment("Value for fog density (0-5) Vanilla is 2.0")
+			  .translation("")
+			  .defineInRange("fogDensityLava", 0.5, 0, 5);
+			
+			builder.pop();
+		}
+		
+	}
+	
+	static final ForgeConfigSpec clientSpec;
+	public static final Client CLIENT;
+	
+	static {
+		final Pair<Client, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Client::new);
+		clientSpec = specPair.getRight();
+		CLIENT = specPair.getLeft();
+	}
 }
